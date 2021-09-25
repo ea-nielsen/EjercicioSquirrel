41ef436a42ef895cd6af214d71ccfa15e8394430
unknown <ea.nielsen@uniandes.edu.co>
2021-09-24 21:15:03 -0500

    falta sort

--- main.js
+++ main.js
@@ -1,16 +1,93 @@
- fetch('https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json')
-.then(response => response.json())
-.then(data => {    
-    let trTag;
+fetch(
+  "https://gist.githubusercontent.com/josejbocanegra/b1873c6b7e732144355bb1627b6895ed/raw/d91df4c8093c23c41dce6292d5c1ffce0f01a68b/newDatalog.json"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    let tbodyRef = document.getElementById("contenidoT1");
+    let tbodyRef2 = document.getElementById("contenidoT2");
+    let dict = {};
     for (let i = 0; i < data.length; i++) {
-        trTag = document.createElement("tr");
-        thIndex = document.createElement("th");
-        thIndex.textContent = i;
-        let events = "";
-        for (let j = 0; j < data[i].events.length; j++) {
-            events += data[i].events[j] +",";
+      let newRow = tbodyRef.insertRow(i);
+      let cel1 = newRow.insertCell(0);
+      cel1.style.fontWeight = "bold";
+      let cel2 = newRow.insertCell(1);
+      let cel3 = newRow.insertCell(2);
+      cel1.innerHTML = i + 1;
+      let events = "";
+      for (let j = 0; j < data[i].events.length; j++) {
+        if (j == data[i].events.length - 1) {
+          events += data[i].events[j];
+        } else {
+          events += data[i].events[j] + ", ";
         }
-        console.log(events);
+        if (!(data[i].events in dict)) {
+          dict[data[i].events[j]] = { TP: 0, TN: 0, FP: 0, FN: 0 };
+        }
+      }
+      cel2.innerHTML = events;
+      cel3.innerHTML = data[i].squirrel;
+      if (data[i].squirrel == true) {
+        newRow.style.background = "#ffc4cc";
+      }
     }
-  
-});
+    for (let i = 0; i < data.length; i++) {
+        for(const key in dict){
+            let encontrado =false;
+            for (let j = 0; j < data[i].events.length; j++) {
+                if(key == data[i].events[j]){
+                    encontrado= true;
+                    if(data[i].squirrel == true){
+                        dict[key]["TP"] = dict[key]["TP"] + 1;
+                    }
+                    else{
+                        dict[key]["FN"] = dict[key]["FN"] + 1;
+                    }
+                }
+            }
+            if(encontrado==false){
+                if(data[i].squirrel == true){
+                    dict[key]["FP"] = dict[key]["FP"] + 1;
+                }
+                else{
+                    dict[key]["TN"] = dict[key]["TN"] + 1;
+                }  
+            }
+        }
+    }
+    console.log(dict);
+
+    for(let i = 0; i < Object.keys(dict).length; i++){
+        let newRow = tbodyRef2.insertRow(i);
+        let cel1 = newRow.insertCell(0);
+        let cel2 = newRow.insertCell(1);
+        let cel3 = newRow.insertCell(2);
+        let eventActual=Object.keys(dict)[i]
+        cel1.innerHTML = i + 1;
+        cel2.innerHTML = eventActual;
+        let arriba = (dict[eventActual]["TP"]*dict[eventActual]["TN"])-(dict[eventActual]["FP"]*dict[eventActual]["FN"]);
+        let abajo = Math.sqrt((dict[eventActual]["TP"]+dict[eventActual]["FP"])*(dict[eventActual]["TP"]+dict[eventActual]["FN"])*(dict[eventActual]["TN"]+dict[eventActual]["FP"])*(dict[eventActual]["TN"]+dict[eventActual]["FN"]));
+        let mcc = arriba/abajo;
+        console.log(mcc);
+        cel3.innerHTML = mcc;
+    }
+    /*
+    for (let i = 0; i < Object.keys(dict).length; i++) {
+        let newRow = tbodyRef2.insertRow(i);
+        let cel1 = newRow.insertCell(0);
+        let cel2 = newRow.insertCell(1);
+        let cel3 = newRow.insertCell(2);
+        let eventActual=Object.keys(dict)[i]
+        cel1.innerHTML = i + 1;
+        cel2.innerHTML = eventActual;
+    console.log(eventActual)
+    let arriba = (dict[eventActual]["TP"]*dict[eventActual]["TN"])-(dict[eventActual]["FP"]*dict[eventActual]["FN"]);
+    let abajo = Math.sqrt((dict[eventActual]["TP"]+dict[eventActual]["FP"])*(dict[eventActual]["TP"]+dict[eventActual]["FN"])*(dict[eventActual]["TN"]+dict[eventActual]["FP"])*(dict[eventActual]["TN"]+dict[eventActual]["FN"]));
+    //console.log("arriba " + eventActual +"es :"+arriba)
+    //console.log("abajo " + eventActual +"es :"+abajo)
+    let  valor =  dict[eventActual]['FN'];
+    
+    }
+    console.log(valor);
+   console.log(dict);
+   */
+  });
